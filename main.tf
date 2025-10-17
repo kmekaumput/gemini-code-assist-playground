2ae991ef149ac1ededf0a7388595d2be8b382224
kmekaumput <kampanartm.careers@gmail.com>
2025-10-17 18:00:32 +1100

    Add main function and test function in main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+def test():
+    return ('a','b','c')
+    
+def main():
+    """Main function to print a greeting."""
+    print('My hello world')
+    abc = test()
+    print(abc)
+
+
+if __name__ == "__main__":
+    main()
+
